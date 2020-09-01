620b35a4106ac885cbb302808cd45e23e07bae61
deyvisonborges <web.dborges@gmail.com>
2020-08-31 23:45:05 -0300

    refactor(navigation): correcao de import

--- src/lib/views/naver/editar-naver/index.js
+++ src/lib/views/naver/editar-naver/index.js
@@ -2,7 +2,7 @@ import React, { useLayoutEffect } from 'react';
 import GenericFormNave from '../../../components/generic-form-naver';
 import HeaderIconBack from '../../../components/header-icon-back';
 
-export default function AdicionarNave() {
+export default function AdicionarNave({ navigation }) {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerLeft: () => <HeaderIconBack goBack={() => navigation.goBack()} />,
